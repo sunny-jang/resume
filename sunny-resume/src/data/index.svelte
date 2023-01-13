629e5737df9a1cd73d2d4f94979f95803c6e6fab
sunny-jang <esunbest@naver.com>
2023-01-13 18:44:32 +0900

    데이터 분리

---
+++ sunny-resume/src/data/index.svelte
@@ -0,0 +1,91 @@
+<script context="module">
+export let name = '장은선';
+export let engName = 'Sunny';
+export let lastUpdated = '2023.01.13';
+
+export let channels = [
+    {
+        label: 'Github',
+        href: 'https://github.com/sunny-jang',
+        text: 'https://github.com/sunny-jang'
+    },
+    {
+        label: 'Blog',
+        href: 'https://sunny-jang.tistory.com',
+        text: 'https://sunny-jang.tistory.com'
+    },
+];
+export let contract = [
+    {
+        label: 'Email',
+        text: 'esunbest@gmail.com'
+    },
+];
+
+export let introduction = [
+    '프로젝트 결과물이 성과를 보였을 때 가장 성취감이 큰',
+    '개발 생산성을 높이는기술을 서비스에 접목시킬 때가 즐거운',
+    '새로운 것을 배우는 것에 두려움이 없는',
+    '한줄의 코드로도 여러 삶에 가치를 더해줄 수 있는',
+]
+
+export let workSection = {
+    workList: [
+        {
+            companyName: 'Pandora.TV',
+            period: '2021-07~ 재직 중',
+            description: '경제에 관심을 가지는 유저들의 블록체인 커뮤니티 web과 mobile 메인 프론트앤드 개발 담당',
+            detailList: [
+                { content: '경제 뉴스 콘텐츠 구독 서비스 개발 및 유지 보수' },
+                { content: '글로벌 IDO 중개 플랫폼 오픈 web3 서비스 운영 및 유지보수' },
+                { content: '사용자 Needs 에 맞는 사용성 개선과 신규 비즈니스 로직 구현' },
+                { content: '레거시 청산 및 기존 비즈니스 유지보수' },
+                { content: '웹 서비스와 포맷이 맞지 않는 앱의 글쓰기에서 웹뷰에 CkEditor를 사용해 글쓰기 기능 통일' },
+                { content: 'typeScirpt, swr 도입으로 코드 프로젝트 퀄리티 개선' },
+            ],
+            techSkils: ['React', 'swr', 'redux-saga', 'less', 'javaScript', 'typeScript']
+        },
+        {
+            companyName: 'Evolve Branding (Canada)',
+            period: '2020-03~2020-09',
+            description: '비지니스 브랜딩을 돕는 캐나다 기업의 인턴십 프로그램의 팀 리더, Flutter 프레임워크를 사용하는 4개 프로젝트 참여',
+            detailList: [
+                { content: 'Flutter/Dart를 사용한 IOS/Android App 제작' },
+                { content: 'Google 번역기 API 라이브러리 이용,  목소리 인식 실시간 번역 서비스' },
+                { content: 'Neumorphic 커스텀 테마 패키지 위젯 개발' },
+            ],
+            techSkils: ['Flutter', 'Dart', 'firebase', 'Hybrid App']
+        },
+        {
+            companyName: 'F9dev',
+            period: '2017-06~2019-01',
+            description: '다양한 서비스의 하이브리드 앱을 의뢰받아 구축 및 설계, 자사 e커머스 ClosedShop의 서비스 운영',
+            detailList: [
+                { content: 'AngularJS, Ionic 을 사용한 WebView Hybrid App 개발' },
+                { content: 'Canvas를 사용하여 데이터를 시각화' },
+                { content: 'Monthly 개발 컨퍼런스에 참여해 구성원으로서 기술로 회사의 비지니스에 더 많이 기여할 수 있는 방향을 고민' },
+                { content: '초기 멤버로써 서비스에 애정을 가지고 고객의 시선에서 경험을 최우선으로 하는 업무 분위기에서 프론트앤드 개발' },
+            ],
+            techSkils: ['AngularJs', 'Ionic', 'javaScript', 'ajax', 'html', 'sass', 'less']
+        },
+        {
+            companyName: '㈜메이젠인터렉트',
+            period: '2017-06~2019-01',
+            description: '공공기관, 공기업, 큰 규모의 다양한 프로젝트의 마크업 개발 ',
+            detailList: [
+                { content: '다수의 프로젝트 웹 접근성 90%→98% 불합격 수정 및 마크 획득' },
+                { content: '접근성이 지켜지지 않은 사이트들에 시맨틱 마크업을 적용해 SEO작업, 접근성 개선' },
+                { content: '브라우저 호환성 준수 등 크로스 브라우징 작업' },
+                { content: 'Mobile First 휴대폰, 테블릿, PC 등의 다양한 디바이스 반응형 작업' },
+                { content: '프로젝트 레이아웃과 페이지 이슈에 대응하는 업무를 진행' },
+            ],
+            techSkils: ['Html', 'css', 'javaScript', 'jQuery']
+        },
+    ]
+
+}
+
+export let projects = {
+
+}
+</script>
