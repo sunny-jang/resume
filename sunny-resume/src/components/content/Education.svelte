fc34e10582a58a404e38e785c0ef8b3db678f34a
sunny-jang <esunbest@naver.com>
2023-01-15 21:41:32 +0900

    교육 관련 컴포넌트 추가

---
+++ sunny-resume/src/components/content/Education.svelte
@@ -0,0 +1,34 @@
+<script>
+    import BigHeader from "../header/BigHeader.svelte";
+
+</script>
+<BigHeader title="EDUCATION" />
+
+    <div class="">
+        <div class="row">
+            <h4>2017. 02 ~ 2022. 02</h4>
+            <div class="group">한양 사이버대학교 (컴퓨터공학과)</div>
+        </div>
+        <div class="row">
+            <h4>2020. 10 ~ 2021. 04</h4>
+            <div class="group">KH정보교육원 (디지털컨버전스 융합 응용SW 개발자)</div>
+        </div>
+    </div>
+<style lang="scss">
+    .row {
+        display: flex;
+        align-items: center;
+        margin-bottom: 10px;
+
+        h4 {
+            margin: 0;
+            margin-right: 30px;
+            font-size: 20px;
+            color: gray;
+        }
+
+        .group {
+            font-weight: bold;
+        }
+    }
+</style>
