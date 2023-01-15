f04b53b6d5389898678ef3a67ce5940b85a3c302
sunny-jang <esunbest@naver.com>
2023-01-15 21:41:23 +0900

    아티클 컴포넌트 추가

---
+++ sunny-resume/src/components/content/Articles.svelte
@@ -0,0 +1,31 @@
+<script>
+    import BigHeader from "../header/BigHeader.svelte";
+
+</script>
+<BigHeader title="Presentation & Article" />
+<div>
+    <hr>
+    <p>(2022. 2. 25) <strong>webpack 라이브러리 걷어내고 커스텀하기</strong></p>
+    <p><a href="https://sunny-jang.tistory.com/47?category=1263281">https://sunny-jang.tistory.com/47?category=1263281</a></p>
+    <p>(2022. 4. 13) <strong>Webpack이란? Webpack 개념, 주요요소(Entry, Output, Loader, Plugin)</strong></p>
+    <p><a href="https://sunny-jang.tistory.com/48">https://sunny-jang.tistory.com/48</a></p>
+    <p>(2023. 1. 11) <strong>useEffect, useState만 사용하지말고 useMemo, useCallback 사용해보자</strong></p>
+    <p><a href="https://sunny-jang.tistory.com/61">https://sunny-jang.tistory.com/61</a></p>
+    <p>(2021. 11. 5) <strong>React/JavaScript 에서 React/TypeScript 마이그레이션 중 발생한 오류</strong></p>
+    <p><a href="https://sunny-jang.tistory.com/42?category=1239166">https://sunny-jang.tistory.com/42?category=1239166</a></p>
+    <p>(2022. 10. 9)<strong>프론트앤드 개발 시 고려사항</strong></p>
+    <p><a href="https://sunny-jang.tistory.com/54">https://sunny-jang.tistory.com/54</a></p>
+    <p>(2022. 10. 17) <strong>웹뷰 개발을 설계하는 방식</strong></p>
+    <p><a href="https://sunny-jang.tistory.com/56">https://sunny-jang.tistory.com/56</a></p>
+    <p>(2022. 8. 22) <strong>Code Seoul 밋업&nbsp;참여!</strong></p>
+    <p><a href="https://sunny-jang.tistory.com/50">https://sunny-jang.tistory.com/50</a></p>
+    <p>(2022. 7. 28) <strong>벌써 1년이나&nbsp;됐다니 1년간의 회고!</strong></p>
+    <p><a href="https://sunny-jang.tistory.com/49">https://sunny-jang.tistory.com/49</a></p>
+    </div>
+
+<style>
+    p {
+        margin: 0;
+        margin-bottom: 5px;
+    }
+</style>
