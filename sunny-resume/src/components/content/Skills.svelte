61af8f3f132422f4c4e6f8b2a425a358d9320d21
sunny-jang <esunbest@naver.com>
2023-01-15 21:41:40 +0900

    스킬 셋 추가

---
+++ sunny-resume/src/components/content/Skills.svelte
@@ -0,0 +1,14 @@
+<script>
+    import BigHeader from "../header/BigHeader.svelte";
+
+</script>
+<BigHeader title="Skills" />
+<div>
+    <p><strong>Frontend</strong></p>
+    <ul>
+        <li>JS|TS(ES6), HTML, CSS(SCSS,LESS), JSP, Dart</li>
+        <li>React, Vue,  Angular, Flutter, Spring</li>
+        <li>webpack, redux, swr, Git</li>
+    </ul>
+</div>
+<style></style>
